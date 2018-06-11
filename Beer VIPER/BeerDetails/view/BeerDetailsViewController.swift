2913b00b14986c5c05d9075722756e5fc06ae422
adrianorezena <adrianorezena@users.noreply.github.com>
2018-06-11 17:10:56 -0300

    add project structure

---
+++ Beer VIPER/BeerDetails/view/BeerDetailsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  BeerDetailsViewController.swift
+//  Beer VIPER
+//
+//  Created by Adriano on 11/06/2018.
+//  Copyright © 2018 Adriano. All rights reserved.
+//
+
+import UIKit
+
+class BeerDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
---
+++ Beer VIPER/FavoriteBeers/view/FavoriteBeersViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FavoritesViewController.swift
+//  Beer VIPER
+//
+//  Created by Adriano on 11/06/2018.
+//  Copyright © 2018 Adriano. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteBeersViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
